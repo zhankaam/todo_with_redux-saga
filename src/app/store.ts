d0607019350fbeff92c2a46b236318aa583250f5
zhankaam <zhankaam@gmail.com>
2021-06-27 13:25:14 +0300

    ;)

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
-import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
-import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
+import {tasksReducer} from '../features/TodolistsList/tasks-reducer'
+import {todolistsReducer} from '../features/TodolistsList/todolists-reducer'
 import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {appReducer} from './app-reducer'
+import {takeEvery} from 'redux-saga/effects'
+import createSagaMiddleware from 'redux-saga'
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -11,10 +13,28 @@ const rootReducer = combineReducers({
     todolists: todolistsReducer,
     app: appReducer
 })
+const sagaMiddleware = createSagaMiddleware()
+
 // непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, sagaMiddleware));
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
+
+sagaMiddleware.run(rootWatcher)
+
+function* rootWatcher() {
+    alert("rootWatcher");
+    yield takeEvery("ACTIVATOR-ACTION-TYPE", rootWorker)
+}
+
+function* rootWorker() {
+    alert("rootWorker");
+}
+
+setTimeout(() => {
+    // @ts-ignore
+    store.dispatch({type: "ACTIVATOR-ACTION-TYPE"})
+},2000)
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
